745cee17774503272fc6368c2cd587d408f1d5b1
Hadi <mhadimollataheri@gmail.com>
2024-01-25 21:29:16 +0330

    Remove some priamry in App.js code and pathes in index.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Ja<span ClassName='diff-color'>mmm</span>ing</h1>
       </header>
     </div>
   );
