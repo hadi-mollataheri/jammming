a2a3c01fc23a3bc24729e2981ee10f6faeb47b09
Hadi <mhadimollataheri@gmail.com>
2024-01-26 17:39:45 +0330

    Import other Child components to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar.js";
+import SearchResult from "../SearchResults/SearchResults.js";
+import Playlist from "../Playlist/Playlist.js";
+import Tracklist from "../Tracklist/Tracklist.js";
+import Track from "../Track/Track.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ja<span ClassName='diff-color'>mmm</span>ing</h1>
+        <h1>
+          Ja<span ClassName="diff-color">mmm</span>ing
+        </h1>
+        {/* <SearchBar />
+        <SearchResult />
+        <Playlist>
+          <Traclist>
+            <Track />
+          </Traclist>
+        </Playlist> */}
       </header>
     </div>
   );
